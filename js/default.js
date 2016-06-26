12dc30f46262ee3c72b287f33b7d43d50f5e8fbd
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-06-26 16:06:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,17 +1,19 @@
 
-var number = Math.floor(Math.random()*21);
-var answer = parseInt(window.prompt('数あてゲーム。0~20の数字を入力してください'));
+var number = Math.floor(Math.random()*10);
+var answer = parseInt(window.prompt('数あてゲーム。0~9の数字を入力してください'));
 
-
+int i=0;
+for(i=0; i<10; i++){
 var message;
 if(answer === number) {
 message = 'あたり';
+break;
 }else if (answer < number){
 message = '残念もっと大きいです';
 }else if (number <answer){
 message = '残念もっと小さいです';
 }else{
-message = '0~3の数字を入力してください';
+message = '0~9の数字を入力してください';
 }
-
+}
 document.getElementById('choice').textContent = message;
